65f7d454d95c5c9d809821feb42aaaf2129dd29b
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-08 16:41:01 +0700

    menggunakan XMLHttpRequest

--- ajaxapi/app-2.js
+++ ajaxapi/app-2.js
@@ -1,3 +1,20 @@
-let jsonContext = '{"nama": "john", "age": 30, "city": "new york"}';
+// let jsonContext = '{"nama": "john", "age": 30, "city": "new york"}';
 
-let dataKu = JSON.parse(jsonContext);
+// let dataKu = JSON.parse(jsonContext);
+
+// -- req via JS dg XMLHttpRequest
+
+const req = new XMLHttpRequest();
+
+let data;
+req.onload = function(){
+    data = JSON.parse(this.responseText);
+    console.log(data);
+};
+
+req.onerror = function(){
+    console.log('error', this);
+};
+
+req.open('GET', 'https://swapi.dev/api/people/11');
+req.send();

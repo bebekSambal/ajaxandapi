396e6fb227850d166a4d96e8619eec4cb7605b33
bebek <155866565+bebekSambal@users.noreply.github.com>
2024-02-11 13:14:26 +0700

    request dg fetch

--- ajaxapi/app-2.js
+++ ajaxapi/app-2.js
@@ -4,17 +4,40 @@
 
 // -- req via JS dg XMLHttpRequest
 
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-let data;
-req.onload = function(){
-    data = JSON.parse(this.responseText);
+// let data;
+// req.onload = function(){
+//     data = JSON.parse(this.responseText);
+//     console.log(data);
+// };
+
+// req.onerror = function(){
+//     console.log('error', this);
+// };
+
+// req.open('GET', 'https://swapi.dev/api/people/11');
+// myReq.setRequestHeader('Accept', 'application/json')
+// req.send();
+
+// -----------------------------------------------------------------
+
+// -- req data dg metod fetch
+// -- cara baru request dari JS
+// -- mendukung promise
+
+const loadPeople = async () =>{
+try {
+    const response = await fetch('https://swapi.dev/api/people/1');
+    const data = await response.json();
     console.log(data);
+    const response2 = await fetch('https://swap.dev/api/people/2');
+    const data2 = await response2.json();
+    console.log(data2);
+} catch (err) {
+    console.log('error :', err);
+}
+
 };
 
-req.onerror = function(){
-    console.log('error', this);
-};
-
-req.open('GET', 'https://swapi.dev/api/people/11');
-req.send();
+loadPeople();
